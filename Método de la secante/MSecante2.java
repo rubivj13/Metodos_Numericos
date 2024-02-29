7c0899d8056c714b94f7282fb2d54e47b878d769
Rubi Veloz Jimenez <147438464+rubivj13@users.noreply.github.com>
2024-02-29 13:45:23 -0600

    Método de la secante
    DESCRIPCIÓN:
    Un problema potencial en la implementación del método de Newton-Raphson es la 
    evaluación de la derivada. Aunque esto no es un inconveniente para los polinomios ni 
    para muchas otras funciones, existen algunas funciones cuyas derivadas en ocasiones 
    resultan muy difíciles de calcular.
    Esta técnica es similar a la del método de Newton-Raphson  en el sentido de que una aproximación de la raíz se predice extrapolando una tangente de la función hasta el eje x. Sin embargo, el método de la secante usa una diferencia dividida en lugar de una derivada para estimar la pendiente.
    
    ALGORITMO: 
    1. Definición del problema:
    - Supongamos que tenemos una función (f(x)) que es continua en un intervalo ([a, b]).
    - Además, (f(a)) y (f(b)) tienen signos opuestos (es decir, (f(a) \cdot f(b) < 0)).
    2. Cálculo de la aproximación inicial:
    - Tomamos dos aproximaciones iniciales: (x_0) y (x_1).
    - La siguiente aproximación, (x_2), se calcula mediante la fórmula: x_2 = x_1 - (f(x_1) (x_1 - x_0)) / (f(x_1) - f(x_0))
    3. Iteración:
    -Repetimos el proceso:
    --Calculamos (x_3) usando (x_1) y (x_2).
    --Calculamos (x_4) usando (x_2) y (x_3).
    --Continuamos hasta alcanzar la precisión deseada.
    4.Convergencia:
    -El método de la secante tiene una convergencia superlineal inferior al método de Newton-Raphson.
    
    IMPLEMENTACIÓN:
    El método de la secante no se tiene en cuenta el signo de la función para estimar el siguiente punto. Se procede independientemente de los signos de la función. De todas maneras en algunos casos es más útil emplear el método de la secante. Este  método,  a  diferencia  del  de  bisección  y  Newton,  casi  nunca  falla  ya  que  solo  requiere  de  2 puntos al principio, y después el mismo método se va retroalimentando.
    
    Para profundizar en la implementación, te invito a explorar los ejercicios que he realizado y que están disponibles en esta plataforma.

---
+++ Método de la secante/MSecante2.java
@@ -0,0 +1,64 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package msecante2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Rub_1
+ */
+public class MSecante2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("\t\t\t\t -- Método de la secante (EJERCICIO 2) --"); 
+        
+        System.out.print("Ingrese Ingrese la primera aproximación: ");
+        double x0 = scanner.nextDouble(); 
+        
+        System.out.print("Ingrese la segunda aproximación: ");
+        double x1 = scanner.nextDouble(); 
+        
+        System.out.print("Ingrese el número máximo de iteraciones: ");
+        int maxIter = scanner.nextInt(); 
+        
+        System.out.print("Ingrese el valor de tolerancia: ");
+        double tol = scanner.nextDouble(); 
+
+        // Se calcula la raíz de la ecuación utilizando el método de la secante
+        double root = secantMethod(x0, x1, maxIter, tol); 
+
+        // La parte %.5f%n sirve para formatear el resultado de la variable root como un número decimal con 5 dígitos después del punto decimal.
+        System.out.printf("La raíz de la ecuación es: %.5f%n", root); 
+    }
+
+    public static double secantMethod(double x0, double x1, int maxIter, double tol) {
+        if (func(x0) * func(x1) == 0) {
+            System.out.println("El método no puede encontrar una raíz en el intervalo."); // Mensaje que se muestra si la raiz no esta dentro de las 2 aproximaciones
+            return -1;
+        }
+
+        double x2 = 0;
+        for (int i = 0; i < maxIter; i++) {
+            x2 = x1 - (func(x1) * (x1 - x0) / (func(x1) - func(x0))); // Calcula la raiz siguendo la formula del método de la secante 
+            if (Math.abs(x1 - x2) < tol) {
+                return x2; // Si el cambio en las aproximaciones es menor que el valor de tolerancia, devolver la aproximación actual como la raíz
+            }
+            x0 = x1; // Actualizar el valor de x0
+            x1 = x2; // Actualizar el valor de x1
+        }
+        return x2; // Si se alcanza el número máximo de iteraciones sin encontrar una raíz con la tolerancia especificada, devolver la última aproximación
+    }
+
+    public static double func(double x) {
+        return 4 * Math.pow(x, 4) - 16; // Definir la función cuya raíz se desea encontrar
+    }
+    
+}
