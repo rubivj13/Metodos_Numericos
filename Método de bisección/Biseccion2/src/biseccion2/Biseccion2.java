6b3f0754e46667d2ab0ec001a22103e6f5eca591
Rubi Veloz Jimenez <147438464+rubivj13@users.noreply.github.com>
2024-02-28 20:51:43 -0600

    Método de la bisección
    El método de bisección, conocido también como de corte binario, de partición de 
    intervalos o de Bolzano, es un tipo de búsqueda incremental en el que el intervalo se 
    divide siempre a la mitad. Si la función cambia de signo sobre un intervalo, se evalúa el 
    valor de la función en el punto medio. La posición de la raíz se determina situándola en 
    el punto medio del subintervalo, dentro del cual ocurre un cambio de signo. El proceso 
    se repite hasta obtener una mejor aproximación.

---
+++ Método de bisección/Biseccion2/src/biseccion2/Biseccion2.java
@@ -0,0 +1,76 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package biseccion2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Rub_1
+ */
+public class Biseccion2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("\t\t\t\t -- Método de bisección (EJERCICIO 2) --");
+
+        System.out.print("Ingrese el límite inferior: ");
+        double a = scanner.nextDouble();
+
+        System.out.print("Ingrese el límite superior: ");
+        double b = scanner.nextDouble();
+
+        System.out.print("Ingrese el número máximo de iteraciones: ");
+        int maxIter = scanner.nextInt();
+
+        System.out.print("Ingrese el valor de tolerancia: ");
+        double tol = scanner.nextDouble();
+
+        // Encontrar la raíz de la función usando el método de bisección
+        double root = bisectionMethod(a, b, maxIter, tol);
+
+        // La parte %.5f%n sirve para formatear el resultado de la variable root como un número decimal con 5 dígitos después del punto decimal.
+        System.out.printf("La raíz de la ecuación es: %.5f%n", root);
+    }
+
+    // Definir el método de bisección
+    public static double bisectionMethod(double a, double b, int maxIter, double tol) {
+        // Verificar si la función cambia de signo en el intervalo
+        if ((func(a) * func(b)) > 0) {
+            System.out.println("El método no puede encontrar una raíz en el intervalo.");
+            return -1;
+        }
+
+        double c;
+        // Iterar hasta que se alcance el número máximo de iteraciones
+        for (int i = 0; i < maxIter; i++) {
+            // Calcular el punto medio del intervalo
+            c = (a + b) / 2;
+            // Verificar si el punto medio está dentro de la tolerancia
+            if (Math.abs(func(c)) < tol) {
+                // Devolver el punto medio como la raíz
+                return c;
+            }
+            // Actualizar el intervalo
+            if (func(a) * func(c) < 0) {
+                b = c;
+            } else {
+                a = c;
+            }
+        }
+        // Devolver el punto medio del intervalo final como la raíz
+        return (a + b) / 2;
+    }
+
+    // Definir la función para encontrar la raíz de
+    public static double func(double x) {
+        return Math.pow(x, 3) - 2 * Math.pow(x, 2) + 4*x + 1;
+    }
+    
+}
